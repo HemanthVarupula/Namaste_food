054805e9c8b20e57a8086be1e781ae72d6b91681
Hemanth Varupula <varupulahemanth@gmailo.com>
2025-04-23 04:03:20 +0530

    episode 4 added

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+export const Header=()=>{
+    return(
+        <div className="header">
+        <div ><img className="logo"src="https://th.bing.com/th?q=Food+Logo+Design+PNG&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-US&cc=US&setlang=en&adlt=moderate&t=1&mw=247"/></div>
+        <div className="nav-items">
+        <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact US</li>
+            <li>Cart</li>
+        </ul>
+        </div>
+        </div>
+    )
+}
