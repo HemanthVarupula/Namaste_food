d717dccef0c0c4aab7dc049553ef39b4d6709440
Hemanth Varupula <varupulahemanth@gmailo.com>
2025-04-23 10:02:39 +0530

    episode 5 added

---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const LOGO_URl="https://th.bing.com/th?q=Food+Logo+Design+PNG&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-US&cc=US&setlang=en&adlt=moderate&t=1&mw=247";
---
+++ src/utils/mockdata.js
@@ -0,0 +1,499 @@
+export const resobj=[
+    {
+        "id": "63048",
+        "name": "Sharief Bhai Biryani",
+        "cloudinaryImageId": "e10544c67ebc6b4c7f82acc5e34b3366",
+        "locality": "Frazer Town",
+        "areaName": "Frazer Town",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "Biryani",
+          "South Indian",
+          "Kebabs",
+          "Ramzan Special",
+          "Sweets",
+          "Beverages"
+        ],
+        "avgRating": 4.3,
+        "parentId": "469102",
+        "avgRatingString": "4.3",
+        "totalRatingsString": "47K+",
+        "promoted": true,
+        "adTrackingId": "cid=28136394~p=3~adgrpid=28136394#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=63048~plpr=COLLECTION~eid=a6e2c48d-be4a-4ab3-b53d-c77fab9e4606~srvts=1745355304401~collid=83639",
+        "sla": {
+          "deliveryTime": 29,
+          "lastMileTravel": 4.7,
+          "serviceability": "SERVICEABLE",
+          "slaString": "25-30 mins",
+          "lastMileTravelString": "4.7 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-04-23 03:00:00",
+          "opened": true
+        },
+        "badges": {},
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "textBased": {},
+            "imageBased": {},
+            "textExtendedBadges": {}
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹200 OFF",
+          "subHeader": "ABOVE ₹799",
+          "discountTag": "FLAT DEAL"
+        },
+        "orderabilityCommunication": {
+          "title": {},
+          "subTitle": {},
+          "message": {},
+          "customIcon": {},
+          "commsStyling": {}
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {},
+            "video": {}
+          }
+        },
+        "reviewsSummary": {},
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {},
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "4.0",
+            "ratingCount": "2.5K+"
+          },
+          "source": "GOOGLE",
+          "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        "campaignId": "28136394"
+      },
+      {
+        "id": "439141",
+        "name": "Biryani Blues",
+        "cloudinaryImageId": "97377e54937c079fe269d744aa66274a",
+        "locality": "Church Street",
+        "areaName": "Church Street",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "Biryani",
+          "Kebabs",
+          "Lucknowi",
+          "Hyderabadi"
+        ],
+        "avgRating": 4.4,
+        "parentId": "13813",
+        "avgRatingString": "4.4",
+        "totalRatingsString": "2.1K+",
+        "sla": {
+          "deliveryTime": 16,
+          "lastMileTravel": 1.9,
+          "serviceability": "SERVICEABLE",
+          "slaString": "15-20 mins",
+          "lastMileTravelString": "1.9 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-04-23 03:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "bolt/bolt%206.png",
+              "description": "bolt!"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "imageId": "bolt/bolt%206.png",
+                    "description": "bolt!"
+                  }
+                }
+              ]
+            },
+            "textExtendedBadges": {},
+            "textBased": {}
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹150 OFF",
+          "subHeader": "ABOVE ₹549",
+          "discountTag": "FLAT DEAL",
+          "logoCtx": {
+            "text": "BENEFITS"
+          }
+        },
+        "orderabilityCommunication": {
+          "title": {},
+          "subTitle": {},
+          "message": {},
+          "customIcon": {},
+          "commsStyling": {}
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {},
+            "video": {}
+          }
+        },
+        "reviewsSummary": {},
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {},
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "4.2",
+            "ratingCount": "350"
+          },
+          "source": "GOOGLE",
+          "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+      {
+        "id": "2415",
+        "name": "Veena Stores",
+        "cloudinaryImageId": "vt1mquaxmeugaf9dl5pp",
+        "locality": "Malleshwaram",
+        "areaName": "Malleshwaram",
+        "costForTwo": "₹100 for two",
+        "cuisines": [
+          "South Indian"
+        ],
+        "avgRating": 4.7,
+        "parentId": "20940",
+        "avgRatingString": "4.7",
+        "totalRatingsString": "81K+",
+        "promoted": true,
+        "adTrackingId": "cid=28163533~p=0~adgrpid=28163533#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=2415~plpr=COLLECTION~eid=7267e8cd-6551-4ac4-ac24-39ee311e6290~srvts=1745379009107~collid=80425",
+        "sla": {
+          "deliveryTime": 37,
+          "lastMileTravel": 6.6,
+          "serviceability": "SERVICEABLE",
+          "slaString": "35-40 mins",
+          "lastMileTravelString": "6.6 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-04-23 11:45:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "v1695133679/badges/Pure_Veg111.png",
+              "description": "pureveg"
+            }
+          ]
+        },
+        "isOpen": true,
+        "aggregatedDiscountInfoV2": {},
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "textBased": {},
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "imageId": "v1695133679/badges/Pure_Veg111.png",
+                    "description": "pureveg"
+                  }
+                }
+              ]
+            },
+            "textExtendedBadges": {}
+          }
+        },
+        "orderabilityCommunication": {
+          "title": {},
+          "subTitle": {},
+          "message": {},
+          "customIcon": {},
+          "commsStyling": {}
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {},
+            "video": {}
+          }
+        },
+    },
+    {
+        "id": "226779",
+        "name": "Chikkanna Tiffin Room",
+        "cloudinaryImageId": "bzc2wmmqdjqcv5efz9er",
+        "locality": "Nagrathpet",
+        "areaName": "Basavanagudi",
+        "costForTwo": "₹200 for two",
+        "cuisines": [
+          "South Indian",
+          "Snacks"
+        ],
+        "avgRating": 4.7,
+        "parentId": "20095",
+        "avgRatingString": "4.7",
+        "totalRatingsString": "692",
+        "sla": {
+          "deliveryTime": 32,
+          "lastMileTravel": 1.4,
+          "serviceability": "SERVICEABLE",
+          "slaString": "30-35 mins",
+          "lastMileTravelString": "1.4 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-04-23 12:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "bolt/bolt%206.png",
+              "description": "bolt!"
+            },
+            {
+              "imageId": "v1695133679/badges/Pure_Veg111.png",
+              "description": "pureveg"
+            }
+          ]
+        },
+        "isOpen": true,
+        "aggregatedDiscountInfoV2": {},
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "textBased": {},
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "imageId": "bolt/bolt%206.png",
+                    "description": "bolt!"
+                  }
+                },
+                {
+                  "attributes": {
+                    "imageId": "v1695133679/badges/Pure_Veg111.png",
+                    "description": "pureveg"
+                  }
+                }
+              ]
+            },
+            "textExtendedBadges": {}
+          }
+        },
+        "orderabilityCommunication": {
+          "title": {},
+          "subTitle": {},
+          "message": {},
+          "customIcon": {},
+          "commsStyling": {}
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {},
+            "video": {}
+          }
+        },
+        "reviewsSummary": {},
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {},
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "4.3",
+            "ratingCount": "2.6K+"
+          },
+          "source": "GOOGLE",
+          "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+      {
+        "id": "335405",
+        "name": "IDC Kitchen",
+        "cloudinaryImageId": "v8jgifosg3vdzrgsv1sw",
+        "locality": "Sampangirama Nagar",
+        "areaName": "Sampangirama Nagar",
+        "costForTwo": "₹150 for two",
+        "cuisines": [
+          "South Indian",
+          "Indian"
+        ],
+        "avgRating": 4.6,
+        "parentId": "105824",
+        "avgRatingString": "4.6",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 18,
+          "lastMileTravel": 1.6,
+          "serviceability": "SERVICEABLE",
+          "slaString": "15-20 mins",
+          "lastMileTravelString": "1.6 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-04-23 21:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "bolt/bolt%206.png",
+              "description": "bolt!"
+            },
+            {
+              "imageId": "v1695133679/badges/Pure_Veg111.png",
+              "description": "pureveg"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "textBased": {},
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "imageId": "bolt/bolt%206.png",
+                    "description": "bolt!"
+                  }
+                },
+                {
+                  "attributes": {
+                    "imageId": "v1695133679/badges/Pure_Veg111.png",
+                    "description": "pureveg"
+                  }
+                }
+              ]
+            },
+            "textExtendedBadges": {}
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "10% OFF",
+          "subHeader": "UPTO ₹40",
+          "logoCtx": {
+            "text": "BENEFITS"
+          }
+        },
+        "orderabilityCommunication": {
+          "title": {},
+          "subTitle": {},
+          "message": {},
+          "customIcon": {},
+          "commsStyling": {}
+        },
+    },
+    {
+        "id": "836274",
+        "name": "Chalukya Samrat Cafe Since 1977",
+        "cloudinaryImageId": "8fa8b191f8d95bde9bc187a85bdb6a02",
+        "locality": "Shanthala Nagar",
+        "areaName": "Central Bangalore",
+        "costForTwo": "₹500 for two",
+        "cuisines": [
+          "Cafe",
+          "Fast Food",
+          "Indian",
+          "Chinese"
+        ],
+        "avgRating": 4.5,
+        "parentId": "496536",
+        "avgRatingString": "4.5",
+        "totalRatingsString": "1.5K+",
+        "sla": {
+          "deliveryTime": 30,
+          "lastMileTravel": 1.4,
+          "serviceability": "SERVICEABLE",
+          "slaString": "30-35 mins",
+          "lastMileTravelString": "1.4 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2025-04-23 23:00:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "bolt/bolt%206.png",
+              "description": "bolt!"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "imageId": "bolt/bolt%206.png",
+                    "description": "bolt!"
+                  }
+                }
+              ]
+            },
+            "textExtendedBadges": {},
+            "textBased": {}
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "25% OFF",
+          "subHeader": "UPTO ₹65",
+          "logoCtx": {
+            "text": "BENEFITS"
+          }
+        },
+        "orderabilityCommunication": {
+          "title": {},
+          "subTitle": {},
+          "message": {},
+          "customIcon": {},
+          "commsStyling": {}
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {},
+            "video": {}
+          }
+        },
+        "reviewsSummary": {},
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {},
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "--"
+          }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+  
+]
